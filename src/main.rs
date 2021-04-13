a24d2a733782c2baaecdd2bdccbcdbf92ac2d435
ZhengYu, Xu <zen-xu@outlook.com>
2021-04-13 23:18:41 +0800

    feat: Init cli app
    Signed-off-by: ZhengYu, Xu <zen-xu@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{crate_name, crate_version, App, AppSettings};
+
+fn build_app() -> App<'static, 'static> {
+    let app = clap_app!(app =>
+        (name: crate_name!())
+        (version: crate_version!())
+        (about: "A tool stitches scripts and commands together by YAML.")
+        (max_term_width: 100)
+        (global_setting: AppSettings::ColoredHelp)
+        (global_setting: AppSettings::UnifiedHelpMessage)
+        (global_setting: AppSettings::HidePossibleValuesInHelp)
+        (setting: AppSettings::ArgsNegateSubcommands)
+        (setting: AppSettings::AllowExternalSubcommands)
+        (setting: AppSettings::DisableHelpSubcommand)
+        (setting: AppSettings::VersionlessSubcommands)
+    );
+
+    app
+}
+
 fn main() {
-    println!("Hello, world!");
+    build_app().get_matches();
 }
