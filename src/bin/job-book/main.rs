c64450a8e31ff7d7b518761c58ba2e3bdcf2ac08
ZhengYu, Xu <zen-xu@outlook.com>
2021-04-29 01:03:50 +0800

    feat: Implement some of mod scheduler
    Signed-off-by: ZhengYu, Xu <zen-xu@outlook.com>

--- src/bin/job-book/main.rs
+++ src/bin/job-book/main.rs
@@ -1,32 +1,63 @@
-extern crate clap;
+// extern crate clap;
+//
+// use clap::{clap_app, crate_name, crate_version, App, AppSettings};
+//
+// fn build_app() -> App<'static, 'static> {
+//     let app = clap_app!(app =>
+//         (name: crate_name!())
+//         (version: crate_version!())
+//         (about: "A tool stitches scripts and commands together by YAML.")
+//         (max_term_width: 100)
+//         (global_setting: AppSettings::ColoredHelp)
+//         (global_setting: AppSettings::UnifiedHelpMessage)
+//         (global_setting: AppSettings::HidePossibleValuesInHelp)
+//         (setting: AppSettings::ArgsNegateSubcommands)
+//         (setting: AppSettings::AllowExternalSubcommands)
+//         (setting: AppSettings::DisableHelpSubcommand)
+//         (setting: AppSettings::VersionlessSubcommands)
+//
+//         (@subcommand run =>
+//             (about: "Execute the given YAML.")
+//             (@arg YAML: +required "The YAML need to run.")
+//             (@arg tags: -t --tag +use_delimiter ... "Execute tasks whose tags are matched.")
+//             (@arg exclude: -e --exclude +use_delimiter  ... "Execute tasks whose tags are not matched.")
+//         )
+//     );
+//
+//     app
+// }
+//
+// fn main() {
+//     build_app().get_matches();
+// }
+use std::fs::File;
+use std::io::{self, Write};
+use std::process::{Command, Stdio};
 
-use clap::{clap_app, crate_name, crate_version, App, AppSettings};
-
-fn build_app() -> App<'static, 'static> {
-    let app = clap_app!(app =>
-        (name: crate_name!())
-        (version: crate_version!())
-        (about: "A tool stitches scripts and commands together by YAML.")
-        (max_term_width: 100)
-        (global_setting: AppSettings::ColoredHelp)
-        (global_setting: AppSettings::UnifiedHelpMessage)
-        (global_setting: AppSettings::HidePossibleValuesInHelp)
-        (setting: AppSettings::ArgsNegateSubcommands)
-        (setting: AppSettings::AllowExternalSubcommands)
-        (setting: AppSettings::DisableHelpSubcommand)
-        (setting: AppSettings::VersionlessSubcommands)
-
-        (@subcommand run =>
-            (about: "Execute the given YAML.")
-            (@arg YAML: +required "The YAML need to run.")
-            (@arg tags: -t --tag +use_delimiter ... "Execute tasks whose tags are matched.")
-            (@arg exclude: -e --exclude +use_delimiter  ... "Execute tasks whose tags are not matched.")
-        )
-    );
-
-    app
-}
+use tempfile::{tempdir, tempfile};
 
 fn main() {
-    build_app().get_matches();
+    use std::fs::{self, File};
+    use std::io::{self, Write};
+    use tempfile::tempdir;
+
+    // Create a directory inside of `std::env::temp_dir()`.
+
+    let result = (|| {
+        let dir = tempdir()?;
+
+        let file_path_buf = dir.path().join("script");
+        let file_path = String::from(file_path_buf.to_str().unwrap());
+        let mut file = File::create(file_path_buf)?;
+
+        writeln!(file, "echo hello")?;
+
+        let mut cmd = Command::new("bash")
+            .arg(file_path)
+            .stdout(Stdio::inherit())
+            .spawn()?;
+        cmd.wait_with_output()
+    })();
+
+    println!("{:?}", result);
 }
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,11 +1,171 @@
-use crate::config::JobConfig;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, Error, Write};
+use std::process::{Command, Output, Stdio};
+
+use tempfile::tempdir;
+
+use crate::config::{self, JobConfig, ParallelTasks, ScriptConfig, TaskConfig, TaskLabels};
+use crate::scheduler::StatusPhase::Running;
 
 pub struct Scheduler {
     job_config: JobConfig,
+    // task_group: TaskGroup,
 }
 
 impl Scheduler {
     fn new(job_config: JobConfig) -> Self {
         Scheduler { job_config }
+    }
+}
+
+enum StatusPhase {
+    Pending,
+    Running,
+    Succeeded,
+    Failed,
+    Skipped,
+}
+
+struct Status {
+    phase: StatusPhase,
+    state: HashMap<StatusPhase, i32>,
+}
+
+struct TaskGroup {
+    task_configs: Vec<ParallelTasks>,
+    tasks: Vec<TaskKind>,
+    status: Status,
+}
+
+enum TaskKind {
+    Task(Task),
+    Template(TaskTemplate),
+}
+
+struct Task {
+    name: String,
+    labels: TaskLabels,
+    script: ScriptConfig,
+    status_phase: StatusPhase,
+    failed_reason: String,
+}
+
+struct TaskTemplate {
+    name: String,
+    labels: TaskLabels,
+    template: String,
+    tasks_group: TaskGroup,
+    status_phase: StatusPhase,
+}
+
+impl Task {
+    fn run<T: Into<Stdio>>(&mut self, stdout_cfg: T) -> io::Result<Output> {
+        let result = (|| {
+            let dir = tempdir()?;
+            let script = dir.path().join("script");
+            let script_name = String::from(script.to_str().unwrap());
+            let mut script = File::create(script)?;
+
+            script.write_all(self.script.source.as_bytes())?;
+
+            let bash_script = format!(
+                r#"
+cd {}
+{} {} {}
+"#,
+                self.script.working_dir,
+                self.script.executor,
+                self.script.executor_opts,
+                script_name
+            );
+
+            let child = Command::new("bash")
+                .arg("-c")
+                .stdout(stdout_cfg)
+                .stderr(Stdio::piped())
+                .arg(&bash_script)
+                .spawn()?;
+
+            self.status_phase = StatusPhase::Running;
+            child.wait_with_output()
+        })();
+
+        match &result {
+            Ok(result) => {
+                if result.status.success() {
+                    self.status_phase = StatusPhase::Succeeded;
+                } else {
+                    self.status_phase = StatusPhase::Failed;
+                    self.failed_reason = String::from_utf8(result.stderr.clone()).unwrap();
+                }
+            }
+            Err(err) => {
+                self.status_phase = StatusPhase::Failed;
+                self.failed_reason = err.to_string();
+            }
+        }
+
+        return result;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_task() {
+        let mut task = Task {
+            name: "echo".to_string(),
+            labels: Default::default(),
+            script: ScriptConfig {
+                source: "echo hello".to_string(),
+                executor: "bash".to_string(),
+                executor_opts: "".to_string(),
+                working_dir: ".".to_string(),
+            },
+            status_phase: StatusPhase::Pending,
+            failed_reason: "".to_string(),
+        };
+
+        let result = task.run(Stdio::piped()).unwrap();
+        assert_eq!(result.stdout.as_slice(), b"hello\n");
+
+        match task.status_phase {
+            StatusPhase::Succeeded => {
+                // ok
+            }
+            _ => {
+                panic!("task should be Succeeded");
+            }
+        }
+    }
+
+    #[test]
+    fn test_failed_task() {
+        let mut task = Task {
+            name: "error".to_string(),
+            labels: Default::default(),
+            script: ScriptConfig {
+                source: "echos hello".to_string(),
+                executor: "bash".to_string(),
+                executor_opts: "".to_string(),
+                working_dir: ".".to_string(),
+            },
+            status_phase: StatusPhase::Pending,
+            failed_reason: "".to_string(),
+        };
+
+        let result = task.run(Stdio::piped());
+
+        match task.status_phase {
+            StatusPhase::Failed => {
+                assert!(task.failed_reason.contains("echos: command not found"))
+            }
+            _ => {
+                panic!("task should be Failed");
+            }
+        }
     }
 }
